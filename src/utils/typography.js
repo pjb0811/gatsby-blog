191d74d2f3e0ee84b79ef456aac5bfc62189d2bb
jbpark <pjb0811@gmail.com>
2018-10-30 08:26:58 +0900

    update typography setting

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,35 +1,32 @@
 import Typography from 'typography'
 import githubTheme from 'typography-theme-github'
 
-const typography = new Typography({
-  ...githubTheme,
-  overrideStyles: ({ rhythm, scale }, options) => {
-    return {
-      html: {
-        height: '100%',
-      },
-      body: {
-        height: '100%',
-      },
-      '#___gatsby': {
-        height: '100%',
-      },
-      '#___gatsby>div': {
-        height: '100%',
-      },
-      a: {
-        color: 'inherit',
-        textDecoration: 'none',
-      },
-      'a.icon': {
-        display: 'inline-flex;',
-      },
-      img: {
-        marginBottom: 0,
-      },
-    }
+githubTheme.overrideThemeStyles = ({ rhythm }, options) => ({
+  html: {
+    height: '100%',
+  },
+  body: {
+    height: '100%',
+  },
+  '#___gatsby': {
+    height: '100%',
+  },
+  '#___gatsby>div': {
+    height: '100%',
+  },
+  a: {
+    color: 'inherit',
+    textDecoration: 'none',
+  },
+  'a.icon': {
+    display: 'inline-flex;',
+  },
+  img: {
+    marginBottom: 0,
   },
 })
+
+const typography = new Typography(githubTheme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
